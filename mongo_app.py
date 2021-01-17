1383e37eef76a888024d7072ad4bea1c6e29ff3d
dianemarie-public <69361953+dianemarie-public@users.noreply.github.com>
2021-01-17 11:07:42 -0600

    Create mongo_app.py

---
+++ mongo_app.py
@@ -0,0 +1,42 @@
+import requests
+from flask import Flask, render_template, redirect
+import pymongo
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Create connection variable
+conn = 'mongodb://localhost:27017'
+
+# Pass connection to the pymongo instance.
+client = pymongo.MongoClient(conn)
+
+url = 'https://data.cityofchicago.org/resource/2ivx-z93u.json'
+
+# Request the json data
+data = requests.get(url).json()
+# print("requesting json data")
+
+# Connect to a database, will create one if not already available
+db = client.beaches_db
+
+# Drops collection if available to remove duplicates
+db.sample.drop()
+
+# Create sample collection in the database and inserts data as documents
+db.sample.insert_many(data)
+
+# Route to render index.html template from Mongo database
+@app.route("/")
+def render_index():
+
+    # dict = {"key_1": "value_1", "key_2": "value_2"}
+
+    # Store the sample collection in a list
+    samples = list(db.sample.find())
+
+    # Return template with dict pass in
+    return render_template("mongo_db.html", samples=samples)
+
+if __name__ == "__main__":
+    app.run(debug=True)
