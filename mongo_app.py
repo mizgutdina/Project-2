31e256702ef3e0670b04b917a5c7f8d788566703
dianemarie-public <69361953+dianemarie-public@users.noreply.github.com>
2021-01-17 12:34:22 -0600

    Update mongo_app.py

--- mongo_app.py
+++ mongo_app.py
@@ -16,16 +16,14 @@ client = pymongo.MongoClient(conn)
 url = 'https://data.cityofchicago.org/resource/2ivx-z93u.json'
 
 # Request the json data
-data = requests.get(url).json()
+lab_data = requests.get(url).json()
 
-# Connect to a database, will create one if not already available
-db = client.beaches_db
-
-# Drops collection if available to remove duplicates
+# Connect to/create labData database, drop sample collection to remove duplicates, create sample collection, insert lab_data from json request
+db = client.labData_db
 db.sample.drop()
+db.sample.insert_many(lab_data)
 
-# Create sample collection in the database and inserts data as documents
-db.sample.insert_many(data)
+
 
 # Route to render index.html template from Mongo database
 @app.route("/")
