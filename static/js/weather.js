57f023be0b960c81c5b1c91ba44555799ffc58c3
Alex Torres <62618236+alextorres12@users.noreply.github.com>
2021-01-19 19:32:29 -0600

    Adds comments to plot.js and weather.js

--- static/js/weather.js
+++ static/js/weather.js
@@ -2,16 +2,12 @@ const city = "chicago";
 
 const weather_url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherAPI_KEY}&units=imperial`;
 
+// pulls in weather data then appends the information to a div using paragraph tags
 d3.json(weather_url).then((data) => {
     var currentTemp = data.main.temp;
     var feelsLike = data.main.feels_like;
     var windSpeed = data.wind.speed;
     var cloudCoverage = data.clouds.all;
-
-    console.log(currentTemp);
-    console.log(feelsLike);
-    console.log(windSpeed);
-    console.log(cloudCoverage);
 
     d3.select("#weather").append("p").text(`Current Temperature: ${currentTemp}°F`);
     d3.select("#weather").append("p").text(`Feels Like: ${feelsLike}°F`);
